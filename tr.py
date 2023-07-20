1f4ef8ee99d8fe0babd297b528d9647ffe38e380
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2023-07-21 01:59:25 +0300

    Create tr.py

---
+++ tr.py
@@ -0,0 +1,27 @@
+# Train the model
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+
+
+# Fit a lasso model on the training set
+model = GradientBoostingRegressor().fit(X_train, y_train)
+print (model, "\n")
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
