4c0fba280c51648fbc41bcb5a07f5d07ddb133f2
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2023-07-20 14:54:14 +0300

    Create sklearn.py

---
+++ sklearn.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import MinMaxScaler
+
+# Get a scaler object
+scaler = MinMaxScaler()
+
+# Create a new dataframe for the scaled values
+df_normalized = df_sample[['Name', 'Grade', 'StudyHours']].copy()
+
+# Normalize the numeric columns
+df_normalized[['Grade','StudyHours']] = scaler.fit_transform(df_normalized[['Grade','StudyHours']])
+
+# Plot the normalized values
+df_normalized.plot(x='Name', y=['Grade','StudyHours'], kind='bar', figsize=(8,5))
